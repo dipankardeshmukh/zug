9b22431f40fb32cc6d7ccc37fdc66fe4f0dfe7cd
sankha <sankha@1e92a999-eb48-914d-8919-15c00e9e00ef>
2012-04-09 12:39:43 +0000

    Updated with latest working copy

---
+++ src/com/automature/zug/engine/CartesianProduct.java
@@ -0,0 +1,129 @@
+/***
+ * http://stackoverflow.com/questions/1719594/iterative-cartesian-product-in-java - Code directly copied from this website with 
+ * very minor modifications.
+ */
+package com.automature.zug.engine;
+
+import java.util.*;
+
+class Tuple<T> {
+    private List<T> list = new ArrayList<T>();
+
+    public void add(T t) { list.add(t); }
+
+    public void addAll(Tuple<T> subT) {
+        for (T t : subT.list) {
+            list.add(t);
+        }
+    }
+    
+    public Object[] ToArray()
+    {
+    	return list.toArray();
+    }
+
+    public String toString() {
+        String result = "(";
+
+        for (T t : list) { result += t + ", "; }
+
+        result = result.substring(0, result.length() - 2);
+        result += " )";
+
+        return result;
+    } 
+}
+
+
+public class CartesianProduct{
+	
+	public static <T> List<Tuple<T>> cartesianProduct(ArrayList<ArrayList<T>> sets) {
+	    List<Tuple<T>> tuples = new ArrayList<Tuple<T>>();
+	    
+	    if(sets.size() == 0)
+	    	return null;
+
+	    if (sets.size() == 1) {
+	        ArrayList<T> set = sets.get(0);
+	        for (T t : set) {
+	            Tuple<T> tuple = new Tuple<T>();
+	            tuple.add(t);    
+	            tuples.add(tuple);
+	        }
+	    } else {
+	        ArrayList<T> set = sets.remove(0);
+	        List<Tuple<T>> subTuples = cartesianProduct(sets);
+	        for (Tuple<T> subTuple : subTuples) {
+	            for (T t : set) {
+	                Tuple<T> tuple = new Tuple<T>();
+	                tuple.add(t);
+	                tuple.addAll(subTuple);
+	                tuples.add(tuple);
+	            }
+	        }
+	    }
+
+	    return tuples;
+	}
+
+	public static <T> List<Tuple<T>> indexedProduct (ArrayList<ArrayList<T>> sets) {
+		List<Tuple<T>> tuples = new ArrayList<Tuple<T>>();
+		
+	    if(sets.size() == 0)
+	    	return null;
+		
+	    if (sets.size() == 1) {
+	        ArrayList<T> set = sets.get(0);
+	        for (T t : set) {
+	            Tuple<T> tuple = new Tuple<T>();
+	            tuple.add(t);    
+	            tuples.add(tuple);
+	        }
+	    } else {
+	        ArrayList<T> set = sets.remove(0);
+	        List<Tuple<T>> subTuples = indexedProduct(sets);
+	        if(set.size()!=1 && subTuples.size()!=1)
+	        {
+	        	for (int i=0;i<subTuples.size();i++)
+		        {
+		        	Tuple<T> tuple = new Tuple<T>();
+		        	tuple.add(set.get(i));
+		        	tuple.addAll(subTuples.get(i));
+		            tuples.add(tuple);
+		        }
+	        }
+	        else if(set.size()==1 && subTuples.size()!=1)
+	        {
+	        	for(int i=0;i<subTuples.size();i++)
+	        	{
+	        		Tuple<T> tuple = new Tuple<T>();
+	        		tuple.add(set.get(0));
+	        		tuple.addAll(subTuples.get(i));
+	        		tuples.add(tuple);
+	        	}
+	        }
+	        else if(set.size()!=1 && subTuples.size()==1)
+	        {
+	        	for(int i=0;i<set.size();i++)
+	        	{
+	        		Tuple<T> tuple = new Tuple<T>();
+	        		tuple.add(set.get(i));
+	        		tuple.addAll(subTuples.get(0));
+	        		tuples.add(tuple);
+	        	}
+	        }
+	        else
+	        {
+	        	Tuple<T> tuple = new Tuple<T>();
+        		tuple.add(set.get(0));
+        		tuple.addAll(subTuples.get(0));
+        		tuples.add(tuple);
+	        }
+	    }
+
+	    return tuples;
+
+	}
+
+
+}
---
+++ src/com/automature/zug/util/XMLWriter.java
@@ -0,0 +1,129 @@
+package com.automature.zug.util;
+
+
+
+	import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+	import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+	import java.io.File;
+	import java.io.FileOutputStream;
+import java.util.HashSet;
+
+	import javax.xml.parsers.DocumentBuilder;
+	import javax.xml.parsers.DocumentBuilderFactory;
+	import org.w3c.dom.Document;
+	import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+
+	/**
+	 *
+	 * @author Sankha
+	 */
+	public class XMLWriter
+	{
+
+	    private static Document doc;
+	   private static Element rootNode;
+	   private static Element includeNode;
+	   private static Integer counter=1;
+
+	    public XMLWriter() {
+	        try { //Document Building Factory
+	            DocumentBuilderFactory docBuildFactory = DocumentBuilderFactory.newInstance();
+	            //Document Builder
+	            DocumentBuilder docBuild = docBuildFactory.newDocumentBuilder();
+	            //Document
+	            doc = docBuild.newDocument();
+	            //Building XML nodes from the given String input of the method parameter Root Element
+	            rootNode = doc.createElement("root");
+	            includeNode=doc.createElement("Include-Files");
+	        } catch (Exception e) {
+	        }
+	    }
+
+	    private Element setAttribute(String name, String value, Document doc) {
+	        //Child Elements
+	        Text fileName = doc.createTextNode(value);
+	        Element moleculeNode = doc.createElement(name);
+	        moleculeNode.setAttribute("id", counter.toString());
+	        moleculeNode.appendChild(fileName);
+	        includeNode.appendChild(moleculeNode);
+	        counter++;
+	        return includeNode;
+	        
+	    }
+
+	    public String genarateXML(String input) {
+	        try {
+	            HashSet<String> inputSets=new HashSet<String>();
+	                String[] filepaths=input.split(","); //Spliting the whole path with ',' for multiple occurrence of files
+	               //Checking and creating the new nodes for getting the folder names
+	                for(String folder:filepaths)
+	                {//Spliting the String inputs with the \ for getting the file name
+	                	String[] fileNames=folder.split("\\\\");//Checking out the file name with the .extensions
+	                	for(String file:fileNames)
+	                	{
+	                		if(file.contains(".xls"))
+	                		{ //Adding a element to the root node
+	                			  //rootNode.appendChild(this.setAttribute("Molecule-File", file, doc));
+	                			inputSets.add(file);
+	                		}
+	                	}
+	                	if(folder.contains(":")||folder.startsWith("\\")) //if the absolute path is given
+	                	//rootNode.appendChild(this.setAttribute("Molecule-Path", folder, doc));
+	                	inputSets.add(folder);
+	                	else
+	                	{
+	                	
+	                			String[] folderName=folder.split("\\\\"); //if relative path is given then it takes the names of folders
+	                			////TODO: need to Come up with some better Logic to stop the repeating Folder name
+	                	//		rootNode.appendChild(this.setAttribute("Molecule-Path", folderName[0], doc));
+	                		inputSets.add(folderName[0]);
+	                	}
+	                	
+	                }
+	               for(Object items:inputSets)
+	               {
+	            	   if(items.toString().contains(".xls"))
+	            		   rootNode.appendChild(this.setAttribute("Molecule-File", items.toString(), doc));
+	            	   else
+	            	      rootNode.appendChild(this.setAttribute("Molecule-Path", items.toString(), doc));
+	            	   
+	               }
+	               
+	                
+
+	        
+	return "Genrated Data for xml";
+	        } catch (Exception s) {
+	            return "This is xml genarating error\t" + s.getLocalizedMessage();
+	        }
+
+	    }
+
+	    public String createXMLFile()
+	    {
+	        try{ 
+	        	 //Finally appending a root node to the main xml document
+	        	doc.appendChild(rootNode);
+	        	
+	        	//Setting a Out put Format
+	            OutputFormat outFormat = new OutputFormat(doc);
+	            outFormat.setIndenting(true);
+	        //Creating the File
+	        File xmlFile = new File("Nyon-Coms.xml");
+	        //FileOutput Stream writing to the file
+	        FileOutputStream xmlOut = new FileOutputStream(xmlFile);
+	        //Xml serializer
+	        XMLSerializer serializer = new XMLSerializer(xmlOut, outFormat);
+	        //Serialize the Data
+	        serializer.serialize(doc);
+	        return "File Is been Created";
+	    }
+	    catch(Exception e) {
+	            return "This is a Problem\t" + e.getLocalizedMessage();
+	    }
+	}
+
+	
+
+}
