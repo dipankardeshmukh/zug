03ee7926489c0637db1b1db38f8243d4ac84e5cc
sankha <sankha@1e92a999-eb48-914d-8919-15c00e9e00ef>
2013-02-13 04:35:35 +0000

    The latest changes for zug 133

--- trunk/src/com/automature/zug/engine/Controller.java
+++ trunk/src/com/automature/zug/engine/Controller.java
@@ -94,7 +94,7 @@ public class Controller extends Thread {
 	private static int repeatDuration = 0;
 	private static double repeatDurationLong = 0;
 	// Change this Number every time the Harness is Released.
-	private static String Version = "ZUG Premium 5.7." + "20130212" + ".132";
+	private static String Version = "ZUG Premium 5.7." + "20130213" + ".133";
 	private static Hashtable<String, String> errorMessageDuringTestCaseExecution = new Hashtable<String, String>();
 	private static Hashtable<String, String> errorMessageDuringMoleculeCaseExecution = new Hashtable<String, String>();
 	private static Hashtable<String, String> threadIdForTestCases = new Hashtable<String, String>();
