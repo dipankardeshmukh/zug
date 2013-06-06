ce5bc4dca8c2505d17f36aa2d70f516520d20766
sarfaraz <sarfaraz@1e92a999-eb48-914d-8919-15c00e9e00ef>
2013-06-06 10:40:14 +0000

    added missing command line param excludetestcaseid in arraylist

--- src/com/automature/zug/engine/ProgramOptions.java
+++ src/com/automature/zug/engine/ProgramOptions.java
@@ -63,7 +63,7 @@ public class ProgramOptions {
 	public static String filelocation = null;
 	private Hashtable<String, String> _opts=new Hashtable<String, String>();
 	protected static String currentPath, workingDirectory;
-	private static final ArrayList<String> commandLineSwitchList=new ArrayList<String>(Arrays.asList("-testcaseid","-repeat","-norepeat","autorecover","-noautorecover","-verbose","-debug","-nodebug","-verify","-noverify","-atompath","-include","-execute","-noexecute","-$","-$$","-testcycleid","-testplan","-testplanid","-topologyset","-topologysetid","-buildtag","-buildid","-macrofile","-macrocolumn","-logfilename","-help","-pwd","-version","-v","-?","--version","--v","-h","--help","/?","-retrycount","-retrytimeout","-testplanname","-buildname","-ignore","-atomexectime"));
+	private static final ArrayList<String> commandLineSwitchList=new ArrayList<String>(Arrays.asList("-testcaseid","-repeat","-norepeat","autorecover","-noautorecover","-verbose","-debug","-nodebug","-verify","-noverify","-atompath","-include","-execute","-noexecute","-$","-$$","-testcycleid","-testplan","-testplanid","-topologyset","-topologysetid","-buildtag","-buildid","-macrofile","-macrocolumn","-logfilename","-help","-pwd","-version","-v","-?","--version","--v","-h","--help","/?","-retrycount","-retrytimeout","-testplanname","-buildname","-ignore","-atomexectime","-excludetestcaseid"));
 	/*
 	 * Creates a new instance.
 	 * 
