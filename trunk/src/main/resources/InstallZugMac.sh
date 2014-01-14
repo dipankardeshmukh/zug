14879dc122f67cfe0904a727536f9e3cd840cab5
soham <soham@1e92a999-eb48-914d-8919-15c00e9e00ef>
2014-01-14 15:19:24 +0000

    updated InstallZugMac.sh by deleting (exit) syntax

--- trunk/src/main/resources/InstallZugMac.sh
+++ trunk/src/main/resources/InstallZugMac.sh
@@ -1,9 +1,12 @@
 #! /usr/bin/env bash
 sudo mv /Applications/Automature/ZUG/zug.mac /Applications/Automature/ZUG/zug
 sudo chmod 755 /Applications/Automature/ZUG/zug
-if [[ $PATH =~ :/Applications/Automature/ZUG: ]]; then
+if [[ $PATH =~ /Applications/Automature/ZUG ]]; then
 	echo "Path alreasy set"
 else
 	sudo echo "/Applications/Automature/ZUG">/etc/paths.d/zug
 fi
-exit
+sudo mkdir $HOME/ZUG\ Logs
+sudo mkdir /Applications/Automature/ZUG/log
+sudo chmod -R a+rw $HOME/ZUG\ Logs
+sudo chmod -R a+rw /Applications/Automature/ZUG/log
