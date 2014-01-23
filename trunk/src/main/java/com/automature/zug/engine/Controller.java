4c636ca334702cf7c507ae5e2a76f5f4e4142401
abhirup <abhirup@1e92a999-eb48-914d-8919-15c00e9e00ef>
2014-01-23 11:20:42 +0000

    Version changed from 7.2.3 to 7.2.4 for #988.

--- trunk/src/main/java/com/automature/zug/engine/Controller.java
+++ trunk/src/main/java/com/automature/zug/engine/Controller.java
@@ -47,7 +47,7 @@ public class Controller extends Thread {
 	public static String logfilename="";
 	static ZugGUI gui;
 	static boolean guiFlag;
-	private static String Version = "ZUG Premium 7.2.3";
+	private static String Version = "ZUG Premium 7.2.4";
 	static Hashtable<String, String[]> fileExtensionSupport;
 
 	public static HashMap<String, String> macrocommandlineinputs = new HashMap<String, String>();
