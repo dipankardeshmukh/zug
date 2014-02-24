dba67fe685b87b78e7295c684b6b391b9bae4814
soham <soham@1e92a999-eb48-914d-8919-15c00e9e00ef>
2014-02-24 11:36:04 +0000

    updated InstallZugLinux.sh, InstallZugMac.sh, SetZugPath.bat with new path and added ZugINI.xml.Linux, ZugINI.xml.Mac, ZugINI.xml.Windows

--- trunk/src/main/resources/InstallZugLinux.sh
+++ trunk/src/main/resources/InstallZugLinux.sh
@@ -1,4 +1,7 @@
 sudo mv /usr/lib/automature/ZUG/zug.linux /usr/lib/automature/ZUG/zug
+sudo mv /usr/lib/automature/ZUG/ZugINI.xml.Linux /usr/lib/automature/ZUG/ZugINI.xml
+sudo rm /usr/lib/automature/ZUG/ZugINI.xml.Mac
+sudo rm /usr/lib/automature/ZUG/ZugINI.xml.Windows
 sudo chmod 755 /usr/lib/automature/ZUG/zug 
 case ":$PATH:" in
   *:/usr/lib/automature/ZUG:*) echo it is in the path;;
--- trunk/src/main/resources/InstallZugMac.sh
+++ trunk/src/main/resources/InstallZugMac.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env bash
 sudo mv /Applications/Automature/ZUG/zug.mac /Applications/Automature/ZUG/zug
+sudo mv /Applications/Automature/ZUG/ZugINI.xml.Mac /Applications/Automature/ZUG/ZugINI.xml
+sudo rm /Applications/Automature/ZUG/ZugINI.xml.Linux
+sudo rm /Applications/Automature/ZUG/ZugINI.xml.Windows
 sudo chmod 755 /Applications/Automature/ZUG/zug
 if [[ $PATH =~ /Applications/Automature/ZUG ]]; then
 	echo "Path alreasy set"
@@ -10,3 +13,5 @@ sudo mkdir $HOME/ZUG\ Logs
 sudo mkdir /Applications/Automature/ZUG/log
 sudo chmod -R a+rw $HOME/ZUG\ Logs
 sudo chmod -R a+rw /Applications/Automature/ZUG/log
+
+
