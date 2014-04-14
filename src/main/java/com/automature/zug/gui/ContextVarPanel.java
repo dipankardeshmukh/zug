af6624e97231e893c16c91533fe66f26b5a4f57f
debabrata <debabrata@1e92a999-eb48-914d-8919-15c00e9e00ef>
2014-04-14 12:53:54 +0000

    JIRA API modified bug 1027 & save context variable to file update

--- src/main/java/com/automature/zug/gui/ContextVarPanel.java
+++ src/main/java/com/automature/zug/gui/ContextVarPanel.java
@@ -5,12 +5,18 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -33,21 +39,21 @@ public class ContextVarPanel extends JPanel {
 	static String value="\n \n \n \n \n \n \n \n \n \n \n";
 	private JButton btnSave=new JButton("SAVE");
 	private ArrayList cvList=null;
-	
+
 	public ContextVarPanel(){
 		initialization();
 	}
-	
+
 	public void initialization(){
 		Dimension d=new Dimension(200,350);
 		setVisible(false);
 		setBackground(Color.white);
 		setMinimumSize(new Dimension(200, 330));
 		setSize(new Dimension(200, 330));
-		
-        cvValueTextArea=new JTextArea();
-        cvValueTextArea.setWrapStyleWord(true);
-        cvValueTextArea.setRows(2);
+
+		cvValueTextArea=new JTextArea();
+		cvValueTextArea.setWrapStyleWord(true);
+		cvValueTextArea.setRows(2);
 		cvValueTextArea.setAutoscrolls(true);
 		cvValueTextArea.setMinimumSize(d);
 		cvValueTextArea.setEditable(false);
@@ -62,22 +68,22 @@ public class ContextVarPanel extends JPanel {
 		scrollPane1.setAutoscrolls(true);
 		scrollPane1.setMinimumSize(new Dimension(200, 300));
 		scrollPane1.setSize(new Dimension(200, 300));
-        JPanel buttonPanel = new JPanel();
+		JPanel buttonPanel = new JPanel();
 		final JButton btnEditCV=new JButton("EDIT");
-        final JButton btnLoadCV=new JButton("LOAD");
-        setLayout(new BorderLayout(0, 0));
+		final JButton btnLoadCV=new JButton("LOAD");
+		setLayout(new BorderLayout(0, 0));
 
-        cvComboBox=new JComboBox();
+		cvComboBox=new JComboBox();
 		add(cvComboBox, BorderLayout.NORTH);
 		add(scrollPane1, BorderLayout.CENTER);
 
-        buttonPanel.add(btnLoadCV);
-        buttonPanel.add(btnEditCV);
-        buttonPanel.add(btnSave);
+		buttonPanel.add(btnLoadCV);
+		buttonPanel.add(btnEditCV);
+		buttonPanel.add(btnSave);
 
-        add(buttonPanel, BorderLayout.SOUTH);
+		add(buttonPanel, BorderLayout.SOUTH);
 		btnSave.setVisible(false);
-		
+
 		cvComboBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				btnSave.setVisible(false);
@@ -87,6 +93,7 @@ public class ContextVarPanel extends JPanel {
 					final int length=value.length();
 					if(length>2000){					
 						cvValueTextArea.setText(value.substring(0, 2000)+"......value truncated");
+						saveFile(value);
 						//ZugGUI.message(msg);
 					}else{
 						cvValueTextArea.setText(value);
@@ -99,7 +106,7 @@ public class ContextVarPanel extends JPanel {
 				cvValueTextArea.setEditable(false);
 			}
 		});
-		
+
 		btnEditCV.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				cvValueTextArea.setEditable(true);
@@ -117,15 +124,60 @@ public class ContextVarPanel extends JPanel {
 			}
 		});
 
-        btnLoadCV.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                refreshList();
-            }
-        });
+		btnLoadCV.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				refreshList();
+			}
+		});
 
 	}
-	
+
+	public File chooseFile(){
+		File file=null;
+
+		JFileChooser fileChooser=new JFileChooser();
+		
+		RecentDirectoryChooserPanel chooser=new RecentDirectoryChooserPanel(ZugGUI.getRecenDirectories(),fileChooser);
+		fileChooser.setAccessory(chooser);
+		int returnVal = fileChooser.showSaveDialog(ContextVarPanel.this);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			file = fileChooser.getSelectedFile();
+		}
+		return file;
+	}
+
+	public void saveFile(String value){
+		
+		int actionDialog = JOptionPane.showConfirmDialog(this, "Save context varible to file");
+		if (actionDialog == JOptionPane.YES_OPTION)
+		{
+			File fileName=chooseFile();
+			if(fileName != null)
+			{
+				if(fileName.exists())
+				{
+					actionDialog = JOptionPane.showConfirmDialog(this, "Replace existing file?");
+					if (actionDialog == JOptionPane.NO_OPTION)
+					{
+						fileName=chooseFile();
+					}
+					              
+				}
+				try {
+					BufferedWriter br=new BufferedWriter(new FileWriter(fileName));
+					br.write(value);
+					br.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}  
+
+			}
+		}
+
+	}
+
 	public void refreshList(){
 		cvList=Controller.getAllContextVariables();
 		cvComboBox.removeAllItems();
