0a125ba860753a720b2a25d42cf9cf6c8c5e3c7b
debabrata <debabrata@1e92a999-eb48-914d-8919-15c00e9e00ef>
2014-04-14 05:52:00 +0000

    Update fix for ZUG GUI freeze problem for long String display (Sarfaraz).

--- src/main/java/com/automature/zug/gui/ContextVarPanel.java
+++ src/main/java/com/automature/zug/gui/ContextVarPanel.java
@@ -1,22 +1,36 @@
 package com.automature.zug.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import javax.swing.*;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.text.AbstractDocument.Content;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultCaret;
+import javax.swing.text.Document;
+import javax.swing.text.PlainDocument;
 
 import com.automature.zug.engine.Controller;
 
-public class ContextVarPanel {
-	public JPanel panel_3;
+public class ContextVarPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
 	JComboBox cvComboBox;
 	JTextArea cvValueTextArea;
+	static String value="\n \n \n \n \n \n \n \n \n \n \n";
 	private JButton btnSave=new JButton("SAVE");
 	private ArrayList cvList=null;
 	
@@ -25,35 +39,43 @@ public class ContextVarPanel {
 	}
 	
 	public void initialization(){
-
-		panel_3 = new JPanel();
-        panel_3.setLayout(new BoxLayout(panel_3, BoxLayout.Y_AXIS));
-		panel_3.setVisible(false);
-		panel_3.setBackground(Color.white);
-
+		Dimension d=new Dimension(200,350);
+		setVisible(false);
+		setBackground(Color.white);
+		setMinimumSize(new Dimension(200, 330));
+		setSize(new Dimension(200, 330));
+		
         cvValueTextArea=new JTextArea();
+        cvValueTextArea.setWrapStyleWord(true);
         cvValueTextArea.setRows(2);
 		cvValueTextArea.setAutoscrolls(true);
+		cvValueTextArea.setMinimumSize(d);
 		cvValueTextArea.setEditable(false);
+		cvValueTextArea.setSize(new Dimension(200, 350));
+		cvValueTextArea.setText(value);
+		cvValueTextArea.setLineWrap(true);
 
 		DefaultCaret caret1 = (DefaultCaret)cvValueTextArea.getCaret();
 		caret1.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
 		final JScrollPane scrollPane1 = new JScrollPane(cvValueTextArea);
 		//scrollPane1.setPreferredSize(new Dimension(300,70));
 		scrollPane1.setAutoscrolls(true);
+		scrollPane1.setMinimumSize(new Dimension(200, 300));
+		scrollPane1.setSize(new Dimension(200, 300));
         JPanel buttonPanel = new JPanel();
 		final JButton btnEditCV=new JButton("EDIT");
         final JButton btnLoadCV=new JButton("LOAD");
+        setLayout(new BorderLayout(0, 0));
 
         cvComboBox=new JComboBox();
-		panel_3.add(cvComboBox);
-		panel_3.add(scrollPane1);
+		add(cvComboBox, BorderLayout.NORTH);
+		add(scrollPane1, BorderLayout.CENTER);
 
         buttonPanel.add(btnLoadCV);
         buttonPanel.add(btnEditCV);
         buttonPanel.add(btnSave);
 
-        panel_3.add(buttonPanel);
+        add(buttonPanel, BorderLayout.SOUTH);
 		btnSave.setVisible(false);
 		
 		cvComboBox.addActionListener(new ActionListener() {
@@ -61,8 +83,14 @@ public class ContextVarPanel {
 				btnSave.setVisible(false);
 				String cvName=(String)cvComboBox.getSelectedItem();
 				if(cvName!=null && !cvName.equals("")){
-					String value=Controller.getContextVarValue(cvName);
-					cvValueTextArea.setText(value);
+					final String value=Controller.getContextVarValue(cvName);
+					final int length=value.length();
+					if(length>2000){					
+						cvValueTextArea.setText(value.substring(0, 2000)+"......value truncated");
+						//ZugGUI.message(msg);
+					}else{
+						cvValueTextArea.setText(value);
+					}
 				}else{
 					cvValueTextArea.setText("");
 				}
@@ -85,7 +113,7 @@ public class ContextVarPanel {
 				String name=(String)cvComboBox.getSelectedItem();
 				String value=cvValueTextArea.getText();
 				Controller.upDateContextVar(name, value);
-				JOptionPane.showMessageDialog(panel_3, "Context Variable value updated");
+				JOptionPane.showMessageDialog(null, "Context Variable value updated");
 			}
 		});
 
@@ -109,5 +137,5 @@ public class ContextVarPanel {
 			}
 		}
 	}
-	
+
 }
