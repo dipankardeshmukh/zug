412e19afd6a0c3d634e455f21faeefcf5787d88e
chayan <chayan@1e92a999-eb48-914d-8919-15c00e9e00ef>
2014-04-25 10:21:35 +0000

    ZugINI.xml file gets replaced only if it don't exist.

--- trunk/src/main/resources/InstallZugLinux.sh
+++ trunk/src/main/resources/InstallZugLinux.sh
@@ -1,5 +1,8 @@
 sudo mv /usr/lib/automature/ZUG/zug.linux /usr/lib/automature/ZUG/zug
+if [ ! -e /usr/lib/automature/ZUG/ZugINI.xml ]
+ then
 sudo mv /usr/lib/automature/ZUG/ZugINI.xml.Linux /usr/lib/automature/ZUG/ZugINI.xml
+fi
 sudo rm /usr/lib/automature/ZUG/ZugINI.xml.Mac
 sudo rm /usr/lib/automature/ZUG/ZugINI.xml.Windows
 sudo chmod 755 /usr/lib/automature/ZUG/zug 
