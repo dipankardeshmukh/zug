db59bd2c2036246f45ad4948ed5d84faf1e7c52b
alok <alok@1e92a999-eb48-914d-8919-15c00e9e00ef>
2016-05-11 09:57:15 +0000

    Spark 1.3.0 installer fixed to run in macosx and ubuntu.

--- trunk/src/main/resources/InstallSparkLinux.sh
+++ trunk/src/main/resources/InstallSparkLinux.sh
@@ -1,8 +1,8 @@
 sudo mv /usr/lib/automature/SPARK/spark.linux /usr/lib/automature/SPARK/spark
-sudo mv /usr/lib/automature/SPARK/zug.linux mv /usr/lib/automature/SPARK/zug
+sudo mv /usr/lib/automature/SPARK/zug.linux /usr/lib/automature/SPARK/zug
 if [ ! -e /usr/lib/automature/SPARK/Spark.ini ]
  then
-sudo mv /usr/lib/automature/SPARK/Spark.ini.Linux /usr/lib/automature/SPARK/Spark.ini.xml
+sudo mv /usr/lib/automature/SPARK/Spark.ini.Linux /usr/lib/automature/SPARK/Spark.ini
 fi
 sudo rm /usr/lib/automature/SPARK/Spark.ini.Mac
 sudo rm /usr/lib/automature/SPARK/Spark.ini.Windows
@@ -13,7 +13,6 @@ case ":$PATH:" in
     sudo echo "PATH=$PATH:/usr/lib/automature/SPARK">/etc/environment
     ;;
 esac
-sudo mkdir $HOME/SPARK\ Logs
-sudo chmod a+rw $HOME/SPARK\ Logs
+sudo mkdir $HOME/SPARK/Logs
+sudo chmod a+rw $HOME/SPARK/Logs
 sudo chmod a+rw /usr/lib/automature/SPARK
-sudo chmod a+rw /usr/lib/automature/SPARK/log
