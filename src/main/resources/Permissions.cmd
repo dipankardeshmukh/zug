d6da058f36a47e9baeb1a41274b73a204d354ca6
alok <alok@1e92a999-eb48-914d-8919-15c00e9e00ef>
2016-06-27 10:42:04 +0000

    Spark version updated to 1.3.1. Fixed installer to have Spark.ini file in the installation directory. Spark.ini file set to "override=false" in izpack during re-installation or upgradation. Fixed installer to set correct installation path during installation.

--- src/main/resources/Permissions.cmd
+++ src/main/resources/Permissions.cmd
@@ -4,8 +4,8 @@ echo %1
 echo %2
 
 :: Set SPARK path
-set spark_path=C:\Program Files\automature
-cd %spark_path%
-set spark_path=%spark_path:"=%
+IF EXIST "C:\Program Files (x86)\Automature\SPARK" (set Spark_Path="C:\Program Files (x86)\Automature") ELSE (set Spark_Path="C:\Program Files\Automature")
+cd %Spark_Path%
+set Spark_Path=%Spark_Path:"=%
 
 icacls "SPARK" /grant Users:F
